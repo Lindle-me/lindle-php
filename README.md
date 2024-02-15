916206bd2981cfce97ff335fa1bad3756d185736
M2K Developments <m2ksoftiez@gmail.com>
2024-02-15 05:48:00 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# lindle-php
-PHP library for Lindle API
+# Lindle PHP
+Lindle is your ultimate solution for managing and organizing your web links with ease. This versatile Chrome extension empowers you to save, categorize, and access your online resources like never before.
+
+<a href="https://www.buymeacoffee.com/m2kdevelopments" target="_blank">
+<img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" />
+</a>
+
+<a href="https://play.google.com/store/apps/details?id=com.m2kdevelopments.lindle" target="_blank">
+    <img src="./googleplay.png" alt="Google Play" style="height: 85px !important" />
+</a>
+<a href="https://chrome.google.com/webstore/detail/igkkojjaikfmiibedalhgmfnjohlhmaj" target="_blank">
+    <img src="./chromestore.png" alt="Chrome Store" style="height: 85px !important" />
+</a>
+
+
+## API Documentation
+The <a href="https://lindle.me/api-docs">API Documentation</a> for Lindle.
+
+
+## Installation
+```
+composer install lindle
+```
+ 
+## Getting Started
+Get your Lindle API key from <a href="https://chrome.google.com/webstore/detail/igkkojjaikfmiibedalhgmfnjohlhmaj">Chrome Extension</a>.
+<br/>
+<img src="./api.png" alt="Chrome Extension" style="height: 400px !important" />
+
+```
+$apiKey = "YOUR_API";
+$lindle = Lindle($apiKey);
+```
+
+## Get Links
+```
+$links = $lindle->getLinks();
+
+```
+
+
+## Get Folders
+```
+$folders = $lindle->getFolders()
+```
+
+
+## Get Synced Bookmarks
+```
+$data = $lindle->getSyncedBookmarks()
+// $data->folders
+// $data->links
+```
---
+++ src/index.php
@@ -0,0 +1,188 @@
+<?php
+
+class Lindle
+{
+    private $api_key;
+
+    public function __construct($api_key)
+    {
+        $this->api_key = $api_key;
+    }
+
+    // GETTING LINKS AND FOLDERS
+
+    public function getUser()
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("GET", "https://www.lindle.me/api/user", $headers), true);
+        return [
+            "id" => $res["_id"],
+            "name" => $res["name"],
+            "image" => $res["image"],
+            "linkLimit" => $res["count"],
+        ];
+    }
+
+    public function getLinks()
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("GET", "https://www.lindle.me/api/links", $headers), true);
+        $links = [];
+        foreach ($res as $item) {
+            $links[] = [
+                "id" => $item["_id"],
+                "name" => $item["name"],
+                "url" => $item["url"],
+                "folder" => $item["folder"] ?? "",
+                "favourite" => $item["favourite"] ?? false,
+            ];
+        }
+        return $links;
+    }
+
+    public function getFolders($with_links = false)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("GET", "https://www.lindle.me/api/folders", $headers), true);
+        $folders = [];
+        $links = $with_links ? $this->getLinks() : [];
+        foreach ($res as $item) {
+            if (!isset($item["public"])) {
+                $item["public"] = false;
+            }
+
+            if (!isset($item["codename"])) {
+                $item["codename"] = "";
+            }
+
+            $folders[] = [
+                "id" => $item["_id"],
+                "name" => $item["name"],
+                "publicFolder" => $item["public"],
+                "journeyLink" => "https://lindle.click/{$item['codename']}",
+                "links" => array_filter($links, function ($link) use ($item) {
+                    return $link["folder"] == $item["_id"];
+                }),
+            ];
+        }
+        return $folders;
+    }
+
+    public function getSyncedBookmarks()
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("GET", "https://www.lindle.me/api/links/bookmarks/sync", $headers), true);
+        $folders = array_map(function ($item) {
+            return [
+                "id" => $item["id"],
+                "name" => $item["name"],
+                "date" => $item["date"],
+                "folder" => $item["folder"],
+            ];
+        }, $res["folders"]);
+
+        $links = array_map(function ($item) {
+            return [
+                "id" => $item["id"],
+                "name" => $item["name"],
+                "date" => $item["date"],
+                "folder" => $item["folder"],
+                "url" => $item["url"],
+            ];
+        }, $res["links"]);
+
+        return ["folders" => $folders, "links" => $links];
+    }
+
+    // CREATING LINKS AND FOLDERS
+
+    public function createLink($name, $url, $folder = null, $favourite = null)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $data = ["name" => $name, "url" => $url, "folder" => $folder, "favourite" => $favourite];
+        $res = json_decode($this->makeRequest("POST", "https://www.lindle.me/api/links", $headers, json_encode($data)), true);
+        $item = $res["link"] ?? [];
+        $link = [
+            "id" => $item["_id"] ?? "",
+            "name" => $item["name"] ?? "",
+            "url" => $item["url"] ?? "",
+            "folder" => $item["folder"] ?? "",
+            "favourite" => $item["favourite"] ?? false,
+        ];
+        return ["message" => $res["message"] ?? "", "result" => $res["result"] ?? "", "link" => $link];
+    }
+
+    public function createFolder($name, $public_folder = null)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $data = ["name" => $name, "public" => $public_folder];
+        $res = json_decode($this->makeRequest("POST", "https://www.lindle.me/api/folders", $headers, json_encode($data)), true);
+        return $res;
+    }
+
+    // UPDATING LINKS AND FOLDERS
+
+    public function updateLink($link_id, $name = null, $url = null, $folder = null, $favourite = null)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $data = ["name" => $name, "url" => $url, "folder" => $folder, "favourite" => $favourite];
+        $res = json_decode($this->makeRequest("PATCH", "https://www.lindle.me/api/links/{$link_id}", $headers, json_encode($data)), true);
+        return $res;
+    }
+
+    public function updateFolder($folder_id, $name = null, $public_folder = null)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $data = ["name" => $name, "public" => $public_folder];
+        $res = json_decode($this->makeRequest("PATCH", "https://www.lindle.me/api/folders/{$folder_id}", $headers, json_encode($data)), true);
+        return $res;
+    }
+
+    // DELETE and REMOVAL
+
+    public function deleteLink($link_id)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("DELETE", "https://www.lindle.me/api/links/{$link_id}", $headers), true);
+        return $res;
+    }
+
+    public function deleteFolder($folder_id)
+    {
+        $headers = ["Authorization" => "Bearer {$this->api_key}"];
+        $res = json_decode($this->makeRequest("DELETE", "https://www.lindle.me/api/folders/{$folder_id}", $headers), true);
+        return $res;
+    }
+
+    private function makeRequest($method, $url, $headers = [], $data = null)
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+
+        if (!empty($headers)) {
+            $curl_headers = [];
+            foreach ($headers as $key => $value) {
+                $curl_headers[] = "{$key}: {$value}";
+            }
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $curl_headers);
+        }
+
+        if ($method === "POST" || $method === "PATCH") {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        }
+
+        $response = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            echo 'Curl error: ' . curl_error($curl);
+        }
+
+        curl_close($curl);
+
+        return $response;
+    }
+}
